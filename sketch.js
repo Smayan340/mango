b0f4f3956e6879d7d982a2905ecb66f63c7b4632
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-02-22 22:03:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;	
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2 = new mango(1120,100,20);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	stone = new stone(250,340,20,20)
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+
+  groundObject.display();
+}
